1d634f760df541bbf6327fbfff2afcc76fdb727a
michelleg06 <michelle.gonzalez.am@gmail.com>
2023-02-06 21:37:07 +0100

    commit to main
    some commands

---
+++ Text.R
@@ -0,0 +1,16 @@
+#################################################################################################### 
+##
+## Project:
+## Script purpose:
+## Encoding: UTF-8
+## Packages:
+## Date:
+## Author: Michelle González Amador
+## Version:
+## Notes v00:
+##
+####################################################################################################
+
+library(tidyverse)
+x <- sample(LETTERS)
+barplot(table(x))
